57f7896fb9511df09c42751af98a856109cedc15
Pedrok <pedro.evang.santos@gmail.com>
2023-03-29 22:53:34 -0300

    Projeto poo dio

--- src/Main.java
+++ src/Main.java
@@ -44,7 +44,7 @@ public class Main {
         System.out.println("Conteúdos Concluídos Pedro: " + devPedro.getConteudosConcluidos());
         System.out.println("XP: " + devPedro.calcularTotalXp() );
 
-        System.out.println("--------");
+        System.out.println("---------");
 
         Dev devEduardo = new Dev();
         devEduardo.setNome("Eduardo");
